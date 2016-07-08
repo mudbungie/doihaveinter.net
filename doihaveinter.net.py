98d965d92d66ba366821ff0d1c30db7ab53b7c1c
mudbungie <mudbungie@gmail.com>
2016-07-08 05:38:07 +0000

    everything seems in place

--- doihaveinter.net.py
+++ doihaveinter.net.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 from bottle import route, run, template
 import datetime, pytz
 
@@ -6,4 +8,4 @@ def index():
 	response='<title>doihaveinter.net</title><h1>Yes</h1>\n as of ' + datetime.datetime.now(tz=pytz.utc).isoformat()
 	return response
 
-run(host='0.0.0.0', port=80)
+run(host='127.0.0.1', port=8000)

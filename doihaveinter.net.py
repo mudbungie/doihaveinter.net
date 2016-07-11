81f025c0dc37d40637098139dff0ad3fe6c0088c
mudbungie <mudbungie@gmail.com>
2016-07-11 04:53:30 +0000

    added /ip route to report incoming ip

--- doihaveinter.net.py
+++ doihaveinter.net.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
-from bottle import route, run, template
+from bottle import route, run, request
 import datetime, pytz
 
 @route('/')
 def index():
-	response='<title>doihaveinter.net</title><h1>Yes</h1>\n as of ' + datetime.datetime.now(tz=pytz.utc).isoformat()
-	return response
+	return '<title>doihaveinter.net</title><h1>Yes</h1>\n as of ' + datetime.datetime.now(tz=pytz.utc).isoformat()
+
+@route('/ip')
+def ip():
+	return request.environ['HTTP_X_REAL_IP']
 
 run(host='127.0.0.1', port=8000)
